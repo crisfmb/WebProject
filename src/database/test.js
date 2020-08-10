2cc49073338cde3fe0daf63ee69c207f8913b810
Cristina Borges <cristinaborges@cristinas-mbp.home>
2020-08-10 20:02:58 +0100

    SQL ADDED

--- src/database/test.js
+++ src/database/test.js
@@ -1,11 +1,11 @@
 const Database = require('./db')
 const createProffy = require('./createProffy')
 
-Database.then(() => {
+Database.then(async (db) => {
     //Inserir dados
 
     proffyValue = {
-        name: 'Maya Borges',
+        name: "Maya Borges",
         avatar: 'https://lh3.googleusercontent.com/ai4ISn_gjXLnvHbVUQBS4gg1Lt0T_ZqTIM0oh92mt63aSv58-gBzeDt5Ua8_Lqjek9rEfPUjqIKd2ZYEAOH1YK-ARVd1wNGDzjXKeFNqtf5r0FGYfkeSW2iYRlwoWkgcCclJAsRzYeYEFFTH2lCS_CxYegy_xTd3HAilSedoX3K_QFKTQFWYQYQl-zJPK32h7-8AWNY0PWmnw3TSBeVmFq_6vqQ2B6JjuG-kth44OHbiE90dT2I3subxA4yI3ZoXHDjLyXiBR2mHdSwX62AOneCyrUxk7YyOvZjeNFqOcgUkvQiRTiQSVMT4rYJwaz3E6mKT0CB2P-pvdBp3ft1gnuCK3-K28IhYzqgsRBb-3wbTeLO-kbTsB_6BTIBr_EitjO09C76zO8UiDiUNSfVZHYIg6RfFHLE1AoiP-CgNZaHQtSXVqP3njKJh1W16mEadU1_rhpRwEWklqibgT9VIgiQ_x-Vhc3ryObNDc9xhJ8FFt7rulk5GDl4nAz34CuVLJjnC-zIKO65r__Qz7eBvweoBa0nTvhhF_l_4wro7eyHkKdtJzYFiAKNFDF83ywEbu6Ovcz0JmMb-UXf-oslm3iSZQFGg4pLGktKgb_8wznwBLH9RJIoHaxiDsPWAhdVKVeLjHdOHO7tNXPtNoMnSqrqhqYTlhCJae8v4wu4PoTlX3BWlcv7aFmxlJ5j9eA=w1630-h2172-no?authuser=0',
         whatsapp: '2199884558',
         bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
@@ -18,7 +18,7 @@ Database.then(() => {
         //proffy id vem pelo banco autoincrement boco
     }
 
-    classScheduleValue = [
+    classScheduleValues = [
         {
             weekday: 1,
             time_from: 720,
@@ -33,7 +33,24 @@ Database.then(() => {
 
     ]
 
+    await createProffy(db, { proffyValue, classValue, classScheduleValues })
+
     //createProffy(db, { proffyValue, classValue, classScheduleValue })
     //Consultar os dados inseridos
+
+    // todos os proffys
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    console.log(selectedProffys)
+
+    //consultar as classes de um determinado professor e trazer junto os dados do professor
+
+    const selectClassesAndProffys = await db.all(`
+    SELECT classes.*, proffys.*
+    FROM proffys
+    JOIN classes ON (classes.proffy_id = proffys.id)
+    WHERE classes.proffy_id = 1;
+
+`)
+    console.log(selectClassesAndProffys)
 
 })
